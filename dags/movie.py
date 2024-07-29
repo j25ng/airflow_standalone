bbfcdc9ab5625d348c29c4c6b0b5f989aef3f5ca
j25ng <jsan2861@gmail.com>
2024-07-29 12:12:11 +0900

    push movie

--- dags/movie.py
+++ dags/movie.py
@@ -3,10 +3,12 @@ from textwrap import dedent
 
 # The DAG object; we'll need this to instantiate a DAG
 from airflow import DAG
+from pprint import pprint
 
 # Operators; we need this to operate!
 from airflow.operators.bash import BashOperator
 from airflow.operators.empty import EmptyOperator
+from airflow.operators.python import PythonOperator
 
 with DAG(
     'movie',
@@ -24,6 +26,18 @@ with DAG(
     tags=['movie'],
 ) as dag:
 
+    def print_context(ds=None, **kwargs):
+        """Print the Airflow context and ds variable from the context."""
+        print("::group::All kwargs")
+        pprint(kwargs)
+        print("::endgroup::")
+        print("::group::Context variable ds")
+        print(ds)
+        print("::endgroup::")
+        return "Whatever you return gets printed in the logs"
+
+    run_this = PythonOperator(task_id="print_the_context", python_callable=print_context)
+    
     # t1, t2 and t3 are examples of tasks created by instantiating operators
     task_start = EmptyOperator(task_id='start')
     task_end = EmptyOperator(task_id='end', trigger_rule="all_done")
@@ -41,3 +55,4 @@ with DAG(
     )
 
     task_start >> task_get_data >> task_save_data >> task_end
+    task_start >> run_this >> task_end
