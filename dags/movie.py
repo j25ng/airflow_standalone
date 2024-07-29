ebce3a68bef269ed03f6c6bb838155dd7ea03fc0
j25ng <jsan2861@gmail.com>
2024-07-29 17:50:41 +0900

    commit

--- dags/movie.py
+++ dags/movie.py
@@ -8,7 +8,10 @@ from pprint import pprint
 # Operators; we need this to operate!
 from airflow.operators.bash import BashOperator
 from airflow.operators.empty import EmptyOperator
-from airflow.operators.python import PythonOperator
+from airflow.operators.python import (
+        PythonOperator,
+        PythonVirtualenvOperator
+    )
 
 # import func
 from movie.api.call import gen_url, req, get_key, req2list, list2df, save2df
@@ -29,29 +32,64 @@ with DAG(
     tags=['movie'],
 ) as dag:
 
+    def print_context(ds=None, **kwargs):
+        """Print the Airflow context and ds variable from the context."""
+        print("::group::All kwargs")
+        print(kwargs)
+        print("::endgroup::")
+        print("::group::Context variable ds")
+        print(ds)
+        print("::endgroup::")
+        return "Whatever you return gets printed in the logs"
+
     def get_data(ds, **kwargs):
         print(ds)
-        pprint(kwargs)
+        print(kwargs)
         print(f"ds_nodash => {kwargs['ds_nodash']}")
         key = get_key()
         print(f"MOVIE_API_KEY => {key}")
         date = kwargs['ds_nodash']
         df = save2df(date)
         print(df.head(5))
+
+    def branch_fun(**kwargs):
+        ld = kwargs['ds_nodash']
+        import os
+        if os.path.exists(f'~/tmp/test_parquet/load_dt={ld}'):
+            return rm_dir 
+        else:
+            return get_data
+    branch_op = BranchPythonOperator(
+        task_id="branch.op",
+        python_callable=branch_fun,
+    )
+
+    run_this = PythonOperator(
+        task_id="print_the_context",
+        python_callable=print_context,
+    )
     
     # t1, t2 and t3 are examples of tasks created by instantiating operators
     task_start = EmptyOperator(task_id='start')
     task_end = EmptyOperator(task_id='end', trigger_rule="all_done")
 
-    task_get_data = PythonOperator(
+    task_get_data = PythonVirtualenvOperator(
         task_id='get.data',
-        python_callable=get_data
+        python_callable=get_data,
+        requirements=["git+https://github.com/j25ng/movie.git@0.2/api"],
+        system_site_packages=False,
     )
     
     task_save_data = BashOperator(
         task_id='save.data',
-        bash_command="""
-        """
+        bash_command='date'
     )
 
-    task_start >> task_get_data >> task_save_data >> task_end
+    rm_dir = BashOperator(
+        task_id='rm.dir',
+        bash_command='rm -rf ~/tmp/test_parquet/load_dt={{ ds_nodash }}'
+    )
+
+    task_start >> branch_op
+    branch_op >> rm_dir >> task_get_data
+    branch_op >> task_get_data >> task_save_data >> task_end
