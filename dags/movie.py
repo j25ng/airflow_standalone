3b0a5b1005edc4bac76330d0408e1b2dae274094
j25ng <jsan2861@gmail.com>
2024-07-31 10:20:22 +0900

    complex

--- dags/movie.py
+++ dags/movie.py
@@ -23,12 +23,12 @@ with DAG(
     # You can override them on a per-task basis during operator initialization
     default_args={
         'depends_on_past': False,
-        'email_on_failure': False,
-        'email_on_retry': False,
         'retries': 1,
         'retry_delay': timedelta(seconds=3)
     },
-    description='movie DAG',
+    max_active_runs=1,
+    max_active_tasks=3,
+    description='movie',
     schedule="10 2 * * *",
     start_date=datetime(2024, 7, 24),
     catchup=True,
